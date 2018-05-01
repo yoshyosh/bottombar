dd7007b780ed00912ef4833d8a704b87547bcf38
= <=>
2018-05-01 16:21:20 -0700

    Add subscribe slide

--- js/social.js
+++ js/social.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
-  var Url = window.location.href;
-  var UrlEncoded = encodeURIComponent(Url);
-  var title = encodeURIComponent(document.getElementById("title").innerText);
-  document.getElementById("fb-share").href="http://www.facebook.com/share.php?u=" + UrlEncoded;
-  document.getElementById("tweet").href="http://twitter.com/home?status=" + title + " " + UrlEncoded;
-  document.getElementById("linkedin").href="http://www.linkedin.com/shareArticle?mini=true&url=" + UrlEncoded + "&title=" + title;
+  $(".js-mobile-form-btn").click(function(){
+    $(".js-mobile-btn-container").addClass("active");
+    $(".email-octopus-form-row input").attr("placeholder", "Enter email");
+    $(".js-email-octopus-form-wrapper").addClass("active");
+  })
+
+  $(".js-mobile-cancel").click(function(){
+    $(".js-mobile-btn-container").removeClass("active");
+    $(".js-email-octopus-form-wrapper").removeClass("active");
+  });
 });
