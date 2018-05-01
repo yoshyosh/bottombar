b70a18d67e5ec45d2cbed6d6d900d917e23949f6
= <=>
2018-05-01 16:45:51 -0700

    Add scroll reveal on click, smoother animations

--- js/social.js
+++ js/social.js
@@ -3,10 +3,19 @@ $(document).ready(function(){
     $(".js-mobile-btn-container").addClass("active");
     $(".email-octopus-form-row input").attr("placeholder", "Enter email");
     $(".js-email-octopus-form-wrapper").addClass("active");
+    $(".js-email-input").focus();
   })
 
   $(".js-mobile-cancel").click(function(){
     $(".js-mobile-btn-container").removeClass("active");
     $(".js-email-octopus-form-wrapper").removeClass("active");
   });
+
+  $(".js-scroll-title").click(function(){
+    revealShareBar();
+  });
 });
+
+function revealShareBar(){
+  $(".super-bottom-bar").addClass("active");
+}
